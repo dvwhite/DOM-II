6b4aa120fe14b0613b6c83f99eba767307429fd0
David White <dwhite0627@gmail.com>
2019-12-06 19:22:56 -0500

    Add mouseover and mouseleave events to nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+// Add event listeners
+
+// mouseover and mouseleave for the nav
+const navContainer = document.querySelector('.main-navigation');
+console.log(navContainer)
+
+navContainer.addEventListener('mouseover', function(){
+    navContainer.style.backgroundColor = 'dodgerblue';
+}, false);
+
+navContainer.addEventListener('mouseleave', function(){
+    navContainer.style.backgroundColor = '#FFFFFF';
+}, false);
