0481ec54fd5651212427f1c784b7d1a49b2c68b1
David White <dwhite0627@gmail.com>
2019-12-06 20:21:48 -0500

    Bold nav links on mouseover

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
+// Helper functions
+function boldNav(navItem) {
+    navItem.addEventListener('mouseover', function() {
+        navItem.style.fontWeight = 'bold';
+    }, false);
+
+    navItem.addEventListener('mouseleave', function() {
+        navItem.style.fontWeight = 'normal';
+    }, false);
+}
+
 // Add event listeners
 
 // mouseover and mouseleave for the nav
 const navContainer = document.querySelector('.main-navigation');
-console.log(navContainer)
 
 navContainer.addEventListener('mouseover', function(){
-    navContainer.style.backgroundColor = 'dodgerblue';
+    navContainer.style.backgroundColor = '#FFEBCD';
 }, false);
 
 navContainer.addEventListener('mouseleave', function(){
     navContainer.style.backgroundColor = '#FFFFFF';
 }, false);
+
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(navLink => boldNav(navLink));
