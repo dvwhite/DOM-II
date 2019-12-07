132a79cdf6dbde26ec532bbb6197b18dfdc378d9
David White <dwhite0627@gmail.com>
2019-12-06 21:29:08 -0500

    Add click and dbl click events

--- js/index.js
+++ js/index.js
@@ -19,24 +19,83 @@ function boldNav(navItem) {
     }, false);
 }
 
+/*
+* Style elements on mouseover
+* @returns: none
+*/
+function mouseOver(){
+    navContainer.style.backgroundColor = '#FFEBCD';
+    navContainer.style.borderBottom = '2px dashed darkgoldenrod';
+    navContainer.style.transition = 'all 0.3s ease';
+}
+
+/*
+* Style elements on mouseleave
+* @returns: none
+*/
+function mouseLeave(){
+    navContainer.style.backgroundColor = '#FFFFFF';
+    navContainer.style.borderBottom = '2px dashed #C0C0C0';
+    navContainer.style.transition = 'all 0.3s ease';
+}
+
+/*
+* Style elements on mousedown
+* @returns: none
+*/
+function mouseDown() {
+    introImg.style.transform = 'scale(1.1)';
+}
+
+
+/*
+* Style elements on mouseup
+* @returns: none
+*/
+function mouseUp() {
+    introImg.style.transform = 'scale(1.0)';
+}
+
+/*
+* Add a dblclick event that styles the element passed in
+* @param {object} element: The element to add event listeners to
+* @param {string} color: The string representing the color
+* @returns: none
+*/
+function colorTextOnDblClick(element, color) {
+    element.addEventListener('dblclick', function() {
+        element.style.color = color;
+    });
+}
+
+/*
+* Add a click event that resets styles on the element passed in
+* @param {object} element: The element to add event listeners to
+* @returns: none
+*/
+function resetColorOnClick(element, color) {
+    element.addEventListener('click', function() {
+        element.style.color = color;
+    });
+}
+
 // Add event listeners
 
 // Add mouseover and mouseleave even listeners to elements in the nav
 const navContainer = document.querySelector('.main-navigation');
-
-navContainer.addEventListener('mouseover', function(){
-    navContainer.style.backgroundColor = '#FFEBCD';
-    navContainer.style.borderBottom = '2px dashed darkgoldenrod';
-    navContainer.style.transition = 'all 0.3s ease';
-}, false);
-
-navContainer.addEventListener('mouseleave', function(){
-    navContainer.style.backgroundColor = '#FFFFFF';
-    navContainer.style.borderBottom = '2px dashed #C0C0C0';
-    navContainer.style.transition = 'all 0.3s ease';
-}, false);
+navContainer.style.zIndex = '1';
+navContainer.addEventListener('mouseover', mouseOver, false);
+navContainer.addEventListener('mouseleave', mouseLeave, false);
 
 const navLinks = document.querySelectorAll('nav a');
 navLinks.forEach(navLink => boldNav(navLink));
 
-// 
+// mousedown and mouseup events
+const introImg = document.querySelector('.intro img');
+introImg.addEventListener('mousedown', mouseDown, false);
+introImg.addEventListener('mouseup', mouseUp, false);
+
+// mouseclick and dblclick events
+const pTags = document.querySelectorAll('.container p');
+pTags.forEach(tag => colorTextOnDblClick(tag, 'darkgoldenrod'));
+pTags.forEach(tag => resetColorOnClick(tag, 'black'));
